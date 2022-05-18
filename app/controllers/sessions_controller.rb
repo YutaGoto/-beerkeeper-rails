179426d21d35cbba1e0e81778830a1149db02ddb
YutaGoto <you.goto.510@gmail.com>
2022-05-19 00:17:44 +0900

    :sparkles: Add Signup api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,4 +15,25 @@ class SessionsController < ApplicationController
       render json: { error: 'UnAuthorized' }, status: :unauthorized
     end
   end
+
+  def signup
+    @user = User.new(user_params)
+    if @user.save
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.zone.now + 24.hours.to_i
+      render json: {
+        token:,
+        exp: time.strftime('%Y-%m-%d %H:%M'),
+        user: @user
+      }, status: :ok
+    else
+      render json: { error: 'UnAuthorized' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation, :name)
+  end
 end
